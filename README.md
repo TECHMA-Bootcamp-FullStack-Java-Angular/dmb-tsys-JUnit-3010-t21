62287c4d101a835160668a99a9632576911913c3
David Maza Borjabad <109163247+d-maza@users.noreply.github.com>
2023-10-31 09:56:34 +0100

    add README

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-TODO README
+<div align="center"><img width="80%"  src="https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-2909-ta03a/blob/main/utils/BootCamp-reduced.gif?raw=true"  alt="Logo BootCamp" /></div>
+<hr>
+
+![My etiqueta](https://img.shields.io/badge/David%20Maza-DiveCode%F0%9F%90%99-blue) ![UD](https://img.shields.io/badge/TA-21-orange)  ![Java Version](https://img.shields.io/badge/JUnic-5.6.2-red)
+
+# UD21 – JUNIT
+
+Este repositorio contiene las tareas del Bootcamp 👨‍💻
+
+Puedes acceder a cada uno de los ejercicios, con su enunciado haciendo clic en el siguiente enlaces:
+
+- [UNIDAD UD21](https://techma-bootcamp-fullstack-java-angular.github.io/dmb-tsys-junit-3010-ta21/)
+
+
---
+++ docs/js/lessons.js
@@ -0,0 +1,31 @@
+export const lessons = [
+  {
+    numLessons: 4,
+    issue: 'Maven',
+    day: 33,
+    msg: '<small> La programción es la única disciplina donde los errores son vistos como algo positivo, “Donald Knuth”',
+    myProgress: '🥚>🐣>🐤>🐓>🕊️>🦃>🦅>🪂>🛩️>✈️>👨‍🚀>🚀>🛸>🦸‍♂️',
+
+    exercise: [
+      {
+        numExercise: 1,
+        title: '',
+        description:`
+        Realiza las pruebas unitárias con JUinit v5 a la clase geometria.dto para obtener como <b> mínimo un 95% de covertura`,
+        urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-JUnit-3010-t21/blob/main/Junit09_Geometria/src.test.java/geometria/dto/test/GeometriaTest.java',
+        urlPG: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-maven-2510-ta20/blob/main/docs/assets/img1.png?raw=true',
+      },
+      {
+        numExercise: 2,
+        title: '',
+        description: `
+        Realiza las pruebas unitárias con JUinit v5 a la clase que tenga <b> todos los metodos que ejecutan la calcualdora </b> "CalculadoraController"`,
+        urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-JUnit-3010-t21/blob/main/calculadora/src/test/java/calculadora/calculadora/AppTest.java',
+        urlPG: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-maven-2510-ta20/blob/main/docs/assets/img2.png?raw=true'
+      }
+    ]
+  },
+]
+
+
+ 
