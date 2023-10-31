c8a2831e44763280d37fb95b71f53446607a2f5a
David Maza Borjabad <109163247+d-maza@users.noreply.github.com>
2023-10-31 10:02:48 +0100

    add images link

--- docs/js/lessons.js
+++ docs/js/lessons.js
@@ -13,7 +13,7 @@ export const lessons = [
         description:`
         Realiza las pruebas unitárias con JUinit v5 a la clase geometria.dto para obtener como <b> mínimo un 95% de covertura`,
         urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-JUnit-3010-t21/blob/main/Junit09_Geometria/src.test.java/geometria/dto/test/GeometriaTest.java',
-        urlPG: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-maven-2510-ta20/blob/main/docs/assets/img1.png?raw=true',
+        urlPG: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-JUnit-3010-t21/blob/main/docs/assets/img1.png?raw=true',
       },
       {
         numExercise: 2,
@@ -21,7 +21,7 @@ export const lessons = [
         description: `
         Realiza las pruebas unitárias con JUinit v5 a la clase que tenga <b> todos los metodos que ejecutan la calcualdora </b> "CalculadoraController"`,
         urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-JUnit-3010-t21/blob/main/calculadora/src/test/java/calculadora/calculadora/AppTest.java',
-        urlPG: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-maven-2510-ta20/blob/main/docs/assets/img2.png?raw=true'
+        urlPG: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-JUnit-3010-t21/blob/main/docs/assets/img2.png?raw=true'
       }
     ]
   },
